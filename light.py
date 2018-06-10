1ec8f64ab91eea0329d0467096081caa3a17dfb9
Dave Iverson <iversondave@gmail.com>
2018-06-10 03:35:39 +0000

    Initial commit: roughed in accelerometer read and lighting in separate files

---
+++ light.py
@@ -0,0 +1,31 @@
+from neopixel import *
+import time
+import atexit
+
+# LED strip configuration:
+LED_COUNT      = 300      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def exit_handler():
+    print("Clearing strip before exit")
+    colorWipe(strip, Color(0,0,0), 5)
+    print("Goodbye")
+
+atexit.register(exit_handler)
+colorWipe(strip, Color(0, 0, 255))
